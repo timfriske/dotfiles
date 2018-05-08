3d646350b09f8256c3953470b8060e19ccd08ef7
Tim Friske <me@tifr.de>
2018-05-08 23:33:40 +0200

    Write about best practices for personal GPG keys
    Explain to the reader and user of the Personal GPG Key Generation
    program how it employs best practices in that domain in order to
    generate very strong and thus secure keys.
    
    Signed-off-by: Tim Friske <me@tifr.de>

--- gnupg/gpg-gen-key-for-user.bash
+++ gnupg/gpg-gen-key-for-user.bash
@@ -4,4 +4,13 @@
 # Tim Friske <me@tifr.de>
 #
 # Generates _OpenPGP_ keys with _GPG_ for personal use for a given user
-# ID.
+# ID following best practices:
+#
+#   . Only one user ID to keep identities separate.
+#   . One key per capability to revoke them independently.
+#   . Favor elliptic curve 25519 over NIST curve key algorithm.
+#   . Favor elliptic curve 25519 over RSA key algorithm.
+#   . One certification-only Ed25519 primary key.
+#   . One signature-only Ed25519 secondary key.
+#   . One encryption-only Cv25519 secondary key.
+#   . One authentication-only Ed25519 secondary key.
