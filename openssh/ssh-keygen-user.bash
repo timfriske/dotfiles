ed9ceac57d325a998e524a5a84ddcc1df8be38d9
Tim Friske <me@tifr.de>
2018-06-13 00:32:20 +0200

    Code helper to generate OpenSSH user keys
    This program should help the user to generate OpenSSH keys with safe
    settings.  It generates two keys, one _Ed25519_ key for high security
    needs and one _RSA-4096_ key for great backwards compatibility.
    
    Signed-off-by: Tim Friske <me@tifr.de>

---
+++ openssh/ssh-keygen-user.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# = OpenSSH user key generation
+# Tim Friske <me@tifr.de>
+#
+# This program generates _OpenSSH_ keys for the current user.  It
+# generates two keys, one _Ed25519_ key and one _RSA-4096_ key.
