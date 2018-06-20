1467c1294ad104112c7000c9810ab99993d5a1e1
Tim Friske <me@tifr.de>
2018-06-20 23:03:07 +0200

    Advise to favor Ed25519 over RSA-4096 OpenSSH key
    Urge the administrator of a host to favor the modern _Ed25519_ key for
    increased security and performance over the dated _RSA-4096_ key for
    great backwards compatibility.
    
    Signed-off-by: Tim Friske <me@tifr.de>

--- openssh/ssh-keygen-host.bash
+++ openssh/ssh-keygen-host.bash
@@ -5,3 +5,8 @@
 #
 # This program generates _OpenSSH_ keys for the current host.  It
 # generates two keys, one _Ed25519_ key and one _RSA-4096_ key.
+#
+# Imprtant note: For increased security and performance you should
+# prefer the modern _Ed25519_ key whenever possible.  For backwards
+# compatibility with older _SSH_ clients and servers you can fallback to
+# the dated but widely supported _RSA-4096_ key.
