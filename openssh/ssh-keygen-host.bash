4690aa4816b80f52ca4a29f4907ab5d5be410c98
Tim Friske <me@tifr.de>
2018-06-20 23:05:14 +0200

    How to generate OpenSSH host keys with sudo
    Give the administrator of a host a tip how s/he can easily generate
    _OpenSSH_ host keys by executing this program with `sudo` to gain
    temporary super user permissions while the program runs instead of `su`
    to gain a permanent `root' shell following the security principle of
    `least privilege'.
    
    Signed-off-by: Tim Friske <me@tifr.de>

--- openssh/ssh-keygen-host.bash
+++ openssh/ssh-keygen-host.bash
@@ -10,3 +10,7 @@
 # prefer the modern _Ed25519_ key whenever possible.  For backwards
 # compatibility with older _SSH_ clients and servers you can fallback to
 # the dated but widely supported _RSA-4096_ key.
+#
+# Tip: Run this program with `sudo` as user `root' to generate _OpenSSH_
+# keys for this host on-the-fly, i.e. without using `su` to to log in as
+# user `root' and out again.
