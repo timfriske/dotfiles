173fff2b5b65864f16a78cd316983f8b4079ae75
Tim Friske <me@tifr.de>
2018-06-20 23:00:08 +0200

    Code helper to generate OpenSSH host keys
    This program should help the administrator of a host to generate
    _OpenSSH_ host keys with safe settings.  It generates two keys, one
    _Ed25519_ key for high security needs and one _RSA-4096_ key for great
    backwards compatibility.
    
    Signed-off-by: Tim Friske <me@tifr.de>

---
+++ openssh/ssh-keygen-host.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# = OpenSSH host key generation
+# Tim Friske <me@tifr.de>
+#
+# This program generates _OpenSSH_ keys for the current host.  It
+# generates two keys, one _Ed25519_ key and one _RSA-4096_ key.
