0f908e6079243eb8c7b90b3b799f190b31966598
Tim Friske <me@tifr.de>
2018-07-16 23:21:29 +0200

    Rephrase rationale behind SSH key gen format
      . Clearly state that by `new file format' _OpenSSH's_ format is meant.
    
      . Make it clear that _OpenSSH's_ new format is already the default for
        _SSH_ _Ed25519_ keys and that for this reason the `-o` flag doesn't
        need to be explicitly given when such a key is generated.  On the
        contrary recommend to specify the `-o` flag for all other types of
        protocol version 2 keys.
    
      . Correctly spell `corrput' as `corrupt'.
    
    Signed-off-by: Tim Friske <me@tifr.de>

--- openssh/ssh-keygen-host.bash
+++ openssh/ssh-keygen-host.bash
@@ -63,10 +63,11 @@ mkdir --verbose --parents -- "$key_folder"
 #      pair in order to be able to identify the key pair and to
 #      differentiate it from others.
 #
-#   5. New-format private key (o): Saves the private key in the new file
-#      format that makes the key harder for attackers to disclose and
-#      corrput.  The `-o` flag is implicitly set for an _Ed25519_ key
-#      but should be explicitly set for any Protocol 2 key.
+#   5. New-format private key (o): Save the private key in _OpenSSH's_
+#      new file format that makes the private key harder for attackers
+#      to disclose and corrupt.  The `-o` flag is already the default
+#      for an _Ed25519_ key but should be explicitly set for any other
+#      protocol version 2 key.
 #
 #   6. New passphrase (N): Uses an empty passphrase, i.e. no passphrase
 #      at all because host keys cannot have one.  Because the file
