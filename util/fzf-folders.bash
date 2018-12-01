463ae4480dd31284ad634f18553c4f1fd1bdb81f
Tim Friske <me@tifr.de>
2018-12-01 15:26:32 +0100

    Let Fzf-folders fuzzy-find folders
      . Find folders with _Fzf-find_.
    
      . Quote folders with special characters in their paths, e.g.
        tabulators, line breaks etc.
    
      . Sort folders by version in ascending order.
    
      . Present the user an interactive search screen that lists the folders
        for her/him to fuzzy-find and pick the ones s/he is interested in.
    
      . Insert the folders into the command line editing buffer of the
        user's _Bash_ shell.
    
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-folders.bash
+++ util/fzf-folders.bash
@@ -3,8 +3,13 @@
 # Tim Friske <me@tifr.de>
 #
 # Dependencies::
-#   _Bash_, _Fzf-insert_
+#   _Bash_, _Fzf-find_, _Fzf-insert_, _Fzf-pick_, _Fzf-quote_,
+#   _Fzf-sort_
 
 function fzf_folders {
-  cmd="" fzf_insert
+  cmd="kind=folder fzf-find \
+    | fzf-quote \
+    | order=version fzf-sort \
+    | cmd='' write=1 fzf-pick" \
+    fzf_insert
 }
