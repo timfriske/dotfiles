322cfa0ae6efd8a0145917d8a243304841cc765b
Tim Friske <me@tifr.de>
2019-03-09 17:43:16 +0100

    Note that Fzf-files utility is for interactive use
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-files.bash
+++ util/fzf-files.bash
@@ -7,6 +7,10 @@
 # version and let the user fuzzy-find and pick one or more of them
 # interactively.
 #
+# Note that this _Bash_ file is intended for interactive use.  It should
+# be sourced by a parent shell and bound to a hot key such as `Ctrl-af`
+# (mnemonic `all files').
+#
 # Dependencies::
 #   _Bash_, _Fzf-find_, _Fzf-insert_, _Fzf-pick_, _Fzf-quote_,
 #   _Fzf-sort_
