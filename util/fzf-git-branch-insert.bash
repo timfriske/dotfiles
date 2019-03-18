b11fee4f0bb840e14cca573a369bb74d89798a7a
Tim Friske <me@tifr.de>
2019-03-18 23:05:05 +0100

    Give overview about Fzf-git-branch-insert
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-git-branch-insert.bash
+++ util/fzf-git-branch-insert.bash
@@ -1,3 +1,6 @@
 # Insert fuzzy-found Git branches into command line
 # =================================================
 # Tim Friske <me@tifr.de>
+#
+# Program to let the user list _Git_ branches, fuzzy-find, pick and
+# perform actions on those s/he is interested in.
