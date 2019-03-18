6f09fee1942122beebfbec3521af217f7aca0ed6
Tim Friske <me@tifr.de>
2019-03-18 23:40:21 +0100

    Explain how Fzf-git-tag-insert disp. branches
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-git-tag-insert.bash
+++ util/fzf-git-tag-insert.bash
@@ -3,7 +3,11 @@
 # Tim Friske <me@tifr.de>
 #
 # Program to let the user list _Git_ tags, fuzzy-find, pick and perform
-# actions on those s/he is interested in.
+# actions on those s/he is interested in.  The tags are represented by
+# their short reference names (refname:short) for quick identification.
+# Each tag is hereby displayed on its own line.  The tags are sorted by
+# version under the assumption that they might contain version
+# identfiers.
 
 function fzf_git_tag_insert {
   cmd="fzf-git-tag" fzf_insert
