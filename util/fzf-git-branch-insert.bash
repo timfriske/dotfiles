c1cafa652969622da3b99af6f55d68c64212dfdc
Tim Friske <me@tifr.de>
2019-03-18 23:17:00 +0100

    Explain how Fzf-git-branch-insert disp. branches
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-git-branch-insert.bash
+++ util/fzf-git-branch-insert.bash
@@ -3,7 +3,11 @@
 # Tim Friske <me@tifr.de>
 #
 # Program to let the user list _Git_ branches, fuzzy-find, pick and
-# perform actions on those s/he is interested in.
+# perform actions on those s/he is interested in.  The branches are
+# represented by their short reference names (refname:short) for quick
+# identification.  Each branch is hereby displayed on its own line.  The
+# branches are sorted by version under the assumption that they might
+# contain version identfiers.
 
 function fzf_git_branch_insert {
   cmd="fzf-git-branch" fzf_insert
