637fb1e901f8ef279d8cc8009fa1c9ee0c56abbd
Tim Friske <me@tifr.de>
2019-03-18 23:37:33 +0100

    Give overview about Fzf-git-tag-insert
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-git-tag-insert.bash
+++ util/fzf-git-tag-insert.bash
@@ -1,3 +1,6 @@
 # Insert fuzzy-found Git tags into command line
 # =============================================
 # Tim Friske <me@tifr.de>
+#
+# Program to let the user list _Git_ tags, fuzzy-find, pick and perform
+# actions on those s/he is interested in.
