739f1eedf8971c1f3ce3933ed21f8adb62a33c98
Tim Friske <me@tifr.de>
2019-03-18 23:50:17 +0100

    Give intended usage ex. for Fzf-git-tag-insert
    Signed-off-by: Tim Friske <me@tifr.de>

--- util/fzf-git-tag-insert.bash
+++ util/fzf-git-tag-insert.bash
@@ -10,7 +10,20 @@
 # identfiers.
 #
 # Note that this program is intended for interactive use.  For quick
-# access it should be bound to a hot key.
+# access it should be bound to a hot key such as `Ctrl-gt` (mnemonic
+# `git tag') as follows:
+#
+# .Intended usage (`~/.bashrc`)
+# ----
+# # Test if fuzzy finder program _Fzf_ is installed.
+# if type -p fzf &> /dev/null; then
+#   source fzf-insert.bash &> /dev/null
+#   if source fzf-git-tag-insert.bash &> /dev/null; then
+#     bind -m vi-insert -x '"\C-gt": "fzf_git_tag_insert"'
+#     bind -m vi-command '"\C-gt": "i\C-gt"'
+#   fi
+# fi
+# ----
 
 function fzf_git_tag_insert {
   cmd="fzf-git-tag" fzf_insert
