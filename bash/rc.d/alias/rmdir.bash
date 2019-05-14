c72898dc17e89342d279778333f5d68c3960eded
Tim Friske <me@tifr.de>
2019-05-14 21:02:12 +0200

    Externalize Bash aliases for Rmdir
    Signed-off-by: Tim Friske <me@tifr.de>

---
+++ bash/rc.d/alias/rmdir.bash
@@ -0,0 +1,11 @@
+# Bash aliases for Rmdir
+# ======================
+# Tim Friske <me@tifr.de>
+
+# Call `rmdir` with default options.
+#
+alias rmdir='rmdir $RMDIR_X_OPTIONS'
+
+# Alias `rd` resembles `cp`, `ln`, `mv` etc.
+#
+alias rd='rmdir'
