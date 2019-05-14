07afa299e8d3382638a444b349f67b2a6aafe95d
Tim Friske <me@tifr.de>
2019-05-14 23:23:25 +0200

    Externalize Bash aliases for Grep
    Signed-off-by: Tim Friske <me@tifr.de>

---
+++ bash/rc.d/alias/grep.bash
@@ -0,0 +1,10 @@
+# Bash aliases for Grep
+# =====================
+# Tim Friske <me@tifr.de>
+
+# Call `grep` with default options.
+#
+alias grep='grep $GREP_X_OPTIONS'
+
+alias egrep='grep --extended-regexp'
+alias fgrep='grep --fixed-strings'
