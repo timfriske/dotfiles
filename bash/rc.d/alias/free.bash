d25cb9eac8afea68a3ea64f4e0b6fcd42337d47a
Tim Friske <me@tifr.de>
2019-05-14 23:00:42 +0200

    Externalize Bash aliases for Free
    Signed-off-by: Tim Friske <me@tifr.de>

---
+++ bash/rc.d/alias/free.bash
@@ -0,0 +1,8 @@
+# Bash aliases for Free
+# =====================
+# Tim Friske <me@tifr.de>
+
+# Show human-readable memory sizes in powers of 1,000 of free and used
+# physical and swap memory.
+#
+alias free='free --human --si'
