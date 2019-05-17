5e16a0f6cb416edeedb6cc1c708635385bcaeded
Tim Friske <me@tifr.de>
2019-05-17 14:52:08 +0200

    Externalize Bash prompt setup
    Signed-off-by: Tim Friske <me@tifr.de>

---
+++ bash/login.d/prompt.bash
@@ -0,0 +1,9 @@
+# Bash prompt setup
+# =================
+# Tim Friske <me@tifr.de>
+
+# Save space for the actual command line by showing only the trailing
+# number of path components of the working directory in the command
+# prompt.  The rest of the path is represented by an ellipsis.
+#
+export PROMPT_DIRTRIM=3
