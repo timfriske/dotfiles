8c6eb7e24f46e32259cac44cc050f2b408b9ec13
Tim Friske <me@tifr.de>
2019-05-27 16:20:04 +0200

    Change location of Bash history file
    Locate it underneath the `~./bash` folder common to most _Bash_ related
    files in order to reduce the overall number of hidden files of various
    programs directly below the user's home directory.
    
    Signed-off-by: Tim Friske <me@tifr.de>

--- bash/login.d/history.bash
+++ bash/login.d/history.bash
@@ -2,11 +2,14 @@
 # =============
 # Tim Friske <me@tifr.de>
 
-# Remember up to 100,000 commands in memory and on disk.  Record the
-# time when each command entered history as an ISO-8601 timestamp.
-# Exclude commands starting with whitespace from history.  Erase all
-# duplicate commands from history before the same command is appended
-# again.
+# Remember up to 100,000 commands in memory and on disk.  Store the
+# history in the `history` file under the `~/.bash` folder where most
+# _Bash_ related files are to be found in order to reduce the number of
+# hidden files of various progams directly below the user's home folder.
+# Record the time when each command entered history as an ISO-8601
+# timestamp.  Exclude commands starting with whitespace from history.
+# Erase all duplicate commands from history before the same command is
+# appended again.
 #
 # By enabling the `$HISTCONTROL` option `erasedups` the history is kept
 # free from duplicate commands which would otherwise clutter up the
@@ -19,6 +22,7 @@
 # Therefore the `$HISTIGNORE` variable should be left unspecified.
 #
 export HISTSIZE=100000
+export HISTFILE=~/.bash/history
 export HISTFILESIZE="$HISTSIZE"
 export HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S '
 export HISTCONTROL='ignorespace:erasedups'
