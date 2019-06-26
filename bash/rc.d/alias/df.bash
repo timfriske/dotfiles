5f0e74b20cb24f1a75877dd869d8b9179deaa7d2
Tim Friske <me@tifr.de>
2019-06-26 23:31:23 +0200

    Rewrite Dff alias as command
    Signed-off-by: Tim Friske <me@tifr.de>

--- bash/rc.d/alias/df.bash
+++ bash/rc.d/alias/df.bash
@@ -5,7 +5,3 @@
 # Show human-readable file system sizes in powers of 1,000.
 #
 alias df='df ${DF_X_OPTIONS:-}'
-
-# Exclude neglectable types of file systems.
-#
-alias dff='df -x devtmpfs -x tmpfs'
