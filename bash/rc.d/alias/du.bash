d3570055bded5e7439186af7b925ca942c83dddd
Tim Friske <me@tifr.de>
2019-06-26 22:27:02 +0200

    Rewrite Dus alias as command
    Signed-off-by: Tim Friske <me@tifr.de>

--- bash/rc.d/alias/du.bash
+++ bash/rc.d/alias/du.bash
@@ -3,7 +3,5 @@
 # Tim Friske <me@tifr.de>
 
 # Show human-readable file and directory sizes in powers of 1,000.
-# Summarize files and directories up to the current directory.
 #
 alias du='du --si'
-alias dus='du --summarize'
