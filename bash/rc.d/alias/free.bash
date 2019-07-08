10644582bb3e7460ab23079f1395d6bf2cd689aa
Tim Friske <me@tifr.de>
2019-07-08 22:52:26 +0200

    Reintegrate aliases for Free command
    Signed-off-by: Tim Friske <me@tifr.de>

--- bash/rc.d/alias/free.bash
+++
@@ -1,8 +0,0 @@
-# Bash aliases for Free
-# =====================
-# Tim Friske <me@tifr.de>
-
-# Show human-readable memory sizes in powers of 1,000 of free and used
-# physical and swap memory.
-#
-alias free='free --human --si'
