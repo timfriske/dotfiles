efd11c03c80393fca94ac07d059fa0a8fa0273cf
Tim Friske <me@tifr.de>
2019-07-08 15:45:20 +0200

    Rewrite Map alias as command
    Signed-off-by: Tim Friske <me@tifr.de>

--- bash/rc.d/alias/xargs.bash
+++
@@ -1,9 +0,0 @@
-# Bash aliases for Xargs
-# ======================
-# Tim Friske <me@tifr.de>
-
-# Run given command on every argument usually read from pipe.  The alias
-# `map` resembles the well-known mathematical term `mapping' also known
-# as `map'.
-#
-alias map='xargs --max-args=1'
