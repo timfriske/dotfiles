ee0ca6b722bfcd492ef72adb58240196a18b2754
Tim Friske <me@tifr.de>
2019-07-08 15:58:19 +0200

    Rewrite Count alias as command
    Signed-off-by: Tim Friske <me@tifr.de>

--- bash/rc.d/alias/sort.bash
+++
@@ -1,8 +0,0 @@
-# Bash aliases for Sort
-# =====================
-# Tim Friske <me@tifr.de>
-
-# Count the number of occurrences and list them in ascending order of
-# their counts.
-#
-alias count='sort | uniq --count | sort --sort=numeric'
